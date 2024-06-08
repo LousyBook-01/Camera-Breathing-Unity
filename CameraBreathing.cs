ec19f99266f167a89e25e14d4ffa263421d98344
LousyBook-01 <lousybook94@gmail.com>
2024-06-08 21:35:40 +0530

    idk
    added camerabreathing script duh

---
+++ CameraBreathing.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class CameraBreathing : MonoBehaviour
+{
+    public Quaternion min;
+    public Quaternion max;
+    public float speed = 0.02f;
+    public float updateDelay = 0.2f;
+    private float time;
+    private Quaternion target;
+    void Update()
+    {
+        if(time > updateDelay)
+        {
+            time = 0f;
+            target = new Quaternion(Random.Range(min.x,max.x), Random.Range(min.y, max.y), Random.Range(min.z, max.z), Random.Range(min.w, max.w));
+        }
+
+        transform.localRotation = Quaternion.Slerp(transform.localRotation, target, speed);
+        time += Time.deltaTime;
+    }
+}
